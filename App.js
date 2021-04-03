096d027ebc130f6f0f7cfab07a14c1defa9ff0d6
Nicolas <oliva.nicolas30@gmail.com>
2021-04-03 20:44:12 -0300

    update components to responsive

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ Basada en la interfaz grafica de Lucia Scott Dribble.
 https://dribbble.com/shots/14709020-Calculator/attachments/6408579?mode=media
 */
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Keyboard, Dimensions, StatusBar, ImageBackground, Vibration, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Dimensions, StatusBar, Vibration, TouchableOpacity } from 'react-native';
 import Icon from '@expo/vector-icons/MaterialCommunityIcons'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
 
 export default function App() {
-  Keyboard.dismiss()
   useEffect(() => {
     return () => {
     }
@@ -17,15 +17,20 @@ export default function App() {
   const c2 = '#d76061'
   const c3 = '#f6f6f7'
 
+  const WIDTH = Dimensions.get('window').width
+  const HEIGTH = Dimensions.get('window').height
+  console.log(WIDTH, HEIGTH);
+
+
   const Button = ({ char, onPress, value, icon, color }) => {
     return (
-      <TouchableOpacity onPress={onPress} activeOpacity={0.8}>
-        <View value={value} style={{ width: 70, height: 70, borderRadius: 10, justifyContent: 'center', alignItems: 'center', margin: 15, backgroundColor: '#22252d' }}>
+      <TouchableOpacity onPress={onPress} activeOpacity={0.5}>
+        <View value={value} style={styles.Button}>
           {
             icon ?
-              <Icon color={color} name={icon} size={35} />
+              <Icon color={color} name={icon} size={hp(4.5)} />
               :
-              <Text style={{ color: color, fontSize: 30 }}>{char}</Text>
+              <Text style={{ color: color, fontSize: hp(4.5)}}>{char}</Text>
           }
         </View>
       </TouchableOpacity>
@@ -38,6 +43,7 @@ export default function App() {
     previousValue: null
   })
 
+  console.log(state);
 
   const handleNumber = (value) => {
     setState({
@@ -101,6 +107,11 @@ export default function App() {
         return calculator()
       case "clear":
         return setState({
+          ...state,
+          currentValue: String(state.currentValue).slice(0, -1),
+        })
+      case 'reset':
+        return setState({
           currentValue: 0,
           operator: null,
           previousValue: null
@@ -121,10 +132,10 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor='#22252d' />
+      <StatusBar backgroundColor='#22252d' barStyle='light-content' />
       <View style={styles.container}>
         <View style={styles.topContainer}>
-          <Text style={{ fontSize: 70, textAlign: 'right', fontWeight: 'bold', color: '#fff', marginRight: 8 }}>{state.currentValue}</Text>
+          <Text style={styles.Label}>{state.currentValue}</Text>
         </View>
         <View style={styles.bottomContainer}>
 
@@ -157,7 +168,7 @@ export default function App() {
           </View>
 
           <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-            <Button icon='restore' color={c3} onPress={() => handleTouch('clear')} />
+            <Button icon='restore' color={c3} onPress={() => handleTouch('reset')} />
             <Button char='0' color={c3} onPress={() => handleTouch('number', 0)} />
             <Button icon='circle-small' color={c3} onPress={() => handleTouch('number', '.')} />
             <Button icon='equal' color={c2} onPress={() => handleTouch('equal')} />
@@ -181,10 +192,26 @@ const styles = StyleSheet.create({
     marginBottom: 20
   },
   bottomContainer: {
-    flex: 1.5,
+    flex: 1.6,
     backgroundColor: '#292d36',
     borderTopLeftRadius: 35,
     borderTopRightRadius: 35,
-    paddingTop: 20
+    paddingTop: 15,
+  },
+  Label:{
+    fontSize: hp(9), 
+    textAlign: 'right', 
+    fontWeight: 'bold', 
+    color: '#fff', 
+    marginRight: 8
+  },
+  Button:{
+    width: wp(20), 
+    height: hp(9.2), 
+    margin:10, 
+    borderRadius: 10, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#22252d' 
   }
 });
